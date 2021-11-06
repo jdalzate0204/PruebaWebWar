08ede01885a1d3e882a409a6dcbd32a89693f321
jdalzate0204 <james.alzate22@hotmail.com>
2021-11-06 17:35:54 -0500

    Avance listar

--- src/main/java/com/mycompany/pruebawebwar/controller/AutorController.java
+++ src/main/java/com/mycompany/pruebawebwar/controller/AutorController.java
@@ -1,31 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.pruebawebwar.controller;
 
+import co.edu.unicundi.pruebaejbjar.dto.AutorDto;
 import co.edu.unicundi.pruebaejbjar.entity.Alumno;
 import co.edu.unicundi.pruebaejbjar.entity.Autor;
+import co.edu.unicundi.pruebaejbjar.exception.BussinessException;
+import co.edu.unicundi.pruebaejbjar.exception.ResourceNotFoundException;
 import co.edu.unicundi.pruebaejbjar.service.IAutorService;
 import co.edu.unicundi.pruebaejbjar.view.VistaAutorLibro;
 import java.util.List;
-import javax.ejb.EJB;
-import javax.ejb.Stateless;
-import javax.validation.Valid;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import javax.ejb.*;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
 
 /**
  *
  * @author acer
  */
-@Stateless //Sin estado
+@Stateless
 @Path("/autores")
 public class AutorController {
     
@@ -35,7 +26,7 @@ public class AutorController {
     
     @POST
     @Path("/guardar")
-    @Consumes(MediaType.APPLICATION_JSON) //Enviar la información en JSON
+    @Consumes(MediaType.APPLICATION_JSON)
     public Response insertar(Autor autor){
         this.service.guardar(autor);
         return Response.status(Response.Status.CREATED).build();
@@ -43,17 +34,50 @@ public class AutorController {
     
     @GET
     @Path("/obtener")
-    @Produces(MediaType.APPLICATION_JSON) //Que se va a retornar
+    @Produces(MediaType.APPLICATION_JSON)
     public Response obtener() {
-        List<Autor> listaAlumno = service.listar();
-        return Response.status(Response.Status.OK).entity(listaAlumno).build();
+        List<Autor> listaAutor = service.listar();
+        return Response.status(Response.Status.OK).entity(listaAutor).build();
+    }
+    
+    @GET
+    @Path("/obtener2")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response obtenerModelMapper() {
+        List<AutorDto> listaAutor = service.listarModelMaper();
+        return Response.status(Response.Status.OK).entity(listaAutor).build();
     }
     
     @GET
     @Path("/obtenerDatosAutor")
-    @Produces(MediaType.APPLICATION_JSON) //Que se va a retornar
+    @Produces(MediaType.APPLICATION_JSON)
     public Response obtenerDatos() {
         List<VistaAutorLibro> lista = service.obtener();
         return Response.status(Response.Status.OK).entity(lista).build();
     }
+    
+    @GET
+    @Path("/obtener/{id}")
+    @Produces(MediaType.APPLICATION_JSON) //Que se va a retornar
+    public Response obtenerPorId(@PathParam("id") Integer id) throws ResourceNotFoundException {
+        Autor autor = service.listarPorId(id);
+        return Response.status(Response.Status.OK).entity(autor).build();
+    }
+    
+    @DELETE
+    @Path("/eliminar/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response eliminar(@PathParam("id") Integer id) throws ResourceNotFoundException {
+          service.eliminar(id);
+          return Response.status(Response.Status.NO_CONTENT).build();
+    }  
+    
+    @PUT
+    @Path("/editar")
+    @Consumes(MediaType.APPLICATION_JSON) //Enviar la información en JSON
+    public Response editar(Autor autor) {
+        this.service.editar(autor);
+        return Response.status(Response.Status.OK).build();
+    }
 }
+
