6dc7532f53968fcb0156b2fa6da6e098764d918e
jdalzate0204 <james.alzate22@hotmail.com>
2021-11-06 23:52:26 -0500

    Se agrego excepcion a handler

--- src/main/java/com/mycompany/pruebawebwar/exception/ExceptionHandler.java
+++ src/main/java/com/mycompany/pruebawebwar/exception/ExceptionHandler.java
@@ -2,12 +2,8 @@ package com.mycompany.pruebawebwar.exception;
 
 import co.edu.unicundi.pruebaejbjar.exception.BussinessException;
 import co.edu.unicundi.pruebaejbjar.exception.ResourceNotFoundException;
-import javax.ws.rs.NotFoundException;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
-import javax.ws.rs.ext.ExceptionMapper;
-import javax.ws.rs.ext.Provider;
+import javax.ws.rs.core.*;
+import javax.ws.rs.ext.*;
 
 /**
  *
@@ -22,32 +18,32 @@ public class ExceptionHandler implements ExceptionMapper<Exception>{
         ex.printStackTrace();
         ExceptionWraper ew;
         
-        if (ex instanceof BussinessException | ex instanceof ArrayIndexOutOfBoundsException) {
-            ew = new ExceptionWraper(Response.Status.BAD_REQUEST.getStatusCode(), Response.Status.BAD_REQUEST.getReasonPhrase(), ex.getMessage(), uriInfo.getPath());
+        if (ex instanceof BussinessException | ex instanceof ArrayIndexOutOfBoundsException | ex instanceof IllegalArgumentException) { //400
+            ew = new ExceptionWraper(Response.Status.BAD_REQUEST.getStatusCode(), 
+                                    Response.Status.BAD_REQUEST.getReasonPhrase(), 
+                                    ex.getMessage(), 
+                                    uriInfo.getPath());
             return Response.status(Response.Status.BAD_REQUEST).entity(ew).build();
-        } else if (ex instanceof ResourceNotFoundException) {
-            ew = new ExceptionWraper(Response.Status.NOT_FOUND.getStatusCode(), Response.Status.NOT_FOUND.getReasonPhrase(), ex.getMessage(), uriInfo.getPath());
+            
+        } else if (ex instanceof ResourceNotFoundException) { //404
+            ew = new ExceptionWraper(Response.Status.NOT_FOUND.getStatusCode(), 
+                                    Response.Status.NOT_FOUND.getReasonPhrase(), 
+                                    ex.getMessage(), 
+                                    uriInfo.getPath());
             return Response.status(Response.Status.NOT_FOUND).entity(ew).build();
-        }else if (ex instanceof CloneNotSupportedException) { //409
+            
+        } else if (ex instanceof CloneNotSupportedException) { //409
             ew = new ExceptionWraper(Response.Status.CONFLICT.getStatusCode(), 
-                                      Response.Status.CONFLICT.getReasonPhrase(), 
-                                      ex.getMessage(), 
-                                      uriInfo.getPath());
+                                    Response.Status.CONFLICT.getReasonPhrase(), 
+                                    ex.getMessage(), 
+                                    uriInfo.getPath());
             return Response.status(Response.Status.CONFLICT).entity(ew).build();
-        }else if (ex instanceof CloneNotSupportedException) { //409
-            ew = new ExceptionWraper(Response.Status.CONFLICT.getStatusCode(), 
-                                      Response.Status.CONFLICT.getReasonPhrase(), 
-                                      ex.getMessage(), 
-                                      uriInfo.getPath());
-            return Response.status(Response.Status.CONFLICT).entity(ew).build();
-        }else if (ex instanceof CloneNotSupportedException) { //409
-            ew = new ExceptionWraper(Response.Status.CONFLICT.getStatusCode(), 
-                                      Response.Status.CONFLICT.getReasonPhrase(), 
-                                      ex.getMessage(), 
-                                      uriInfo.getPath());
-            return Response.status(Response.Status.CONFLICT).entity(ew).build();
-        }else {
-            ew = new ExceptionWraper(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(), Response.Status.INTERNAL_SERVER_ERROR.getReasonPhrase(), "", uriInfo.getPath());
+            
+        }else { //500
+            ew = new ExceptionWraper(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(), 
+                                    Response.Status.INTERNAL_SERVER_ERROR.getReasonPhrase(), 
+                                    "", 
+                                    uriInfo.getPath());
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ew).build();
         }
     }
